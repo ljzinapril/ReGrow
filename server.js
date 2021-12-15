bf0e468d528229ec34fa53195e7d044e744582cb
Jingzhu Li <jli3289@gatech.edu>
2021-12-15 14:51:46 -0500

    started
    ok~

---
+++ server.js
@@ -0,0 +1,10 @@
+//express server
+const express=require('express');
+const app=express();
+
+app.get('/', (req, res)=>res.send('API Running'));
+
+//listen port
+const PORT=process.env.PORT|| 5000;
+app.listen(PORT, ()=> console.log(`Server Started on Port ${PORT}`));
+
